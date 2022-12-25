9cea788cb9a45b0f20431297ecb6caf22bdc570d
Krzysztof Zakrzewski <krzysztof.zakrzewski@protonmail.com>
2022-12-25 23:44:04 +0100

    take all important parts from HTML to JS

---
+++ script.js
@@ -0,0 +1,17 @@
+const currentNumber = document.querySelector('.currcurrentNumber');
+
+const previusNumber = document.querySelector('.previusNumber p');
+
+const mathSihg = document.querySelector('.mathSihg');
+
+const numbersButtons = document.querySelectorAll('.number');
+
+const operatorsButtons = document.querySelectorAll('.operator');
+
+const equalsButton = document.querySelector('.equals');
+
+const clearButton = document.querySelector('.clear');
+
+const calculatorHistory = document.querySelector('.history');
+
+const historyBtn = document.querySelector('.history-btn')
